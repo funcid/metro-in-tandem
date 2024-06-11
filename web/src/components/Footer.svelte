8e1e07b837afa42f8b1a2e38881f14b8274f3a86
funcid <rtem903134@gmail.com>
2024-06-11 04:00:53 +0300

    fix footer link

--- web/src/components/Footer.svelte
+++ web/src/components/Footer.svelte
@@ -1,10 +1,6 @@
-<script>
-    import { page } from '$app/stores';  
-</script>
-
 <div class="flex justify-between font-onest">
     <div class="text-left">
-        <a>{$page.url.host}, {new Date().getFullYear()}</a><br/>
+        <a href="/#">метровтандеме.рф, {new Date().getFullYear()}</a><br/>
         Сделано для хакатона ЛЦТ<br/>
     </div>
     <div class="text-right">
