f96f2d38747233a22a6af957d2d52ae35b9e3f2b
Artem Tsaryuk <42806772+funcid@users.noreply.github.com>
2024-06-16 19:49:27 +0300

    update design

--- web/src/components/passengers/Passengers.svelte
+++ web/src/components/passengers/Passengers.svelte
@@ -92,10 +92,10 @@
                 {#each filteredPassengers as pas}
                     <div class="flex">
                         <div 
-                            class="w-full bg-white ml-[12rem] mb-[12rem] border border-gray-300 shadow-md cursor-pointer hover:bg-gray-100 rounded-[20rem] p-[20rem] rounded-r-none border-r-0"
+                            class="w-full bg-white ml-[12rem] mb-[12rem] border border-gray-300 shadow-md cursor-pointer hover:bg-gray-100 rounded-[20rem] rounded-r-none border-r-0"
                             on:click={() => handleClick(pas.id)}
                         >
-                            <div class="flex justify-between items-center">
+                            <div class="flex justify-between items-center p-[20rem]">
                                 <div class="flex flex-col gap-[10rem]">
                                     <div class="flex items-center">
                                         {pas.fullName}
@@ -120,7 +120,7 @@
                             </div>
                         </div>
                         <a class="
-                            flex bg-gray-500 hover:bg-gray-400 mb-[12rem] border border-gray-300 shadow-md cursor-pointer mb-[12rem] 
+                            flex bg-gray-500 hover:bg-gray-400 border border-gray-300 shadow-md cursor-pointer mb-[12rem] 
                             min-w-[120rem] min-h-[120rem] rounded-[20rem] rounded-l-none border-l-0
                             text-center items-center text-white text-[20rem] uppercase"
                             href={`/#/create-application/${pas.id}`}
