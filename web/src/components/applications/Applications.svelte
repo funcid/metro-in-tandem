1cb93c7221d502158cf1fc0832c5eacdf461a43f
funcid <rtem903134@gmail.com>
2024-06-24 05:40:43 +0300

    add full fio

--- web/src/components/applications/Applications.svelte
+++ web/src/components/applications/Applications.svelte
@@ -159,7 +159,7 @@
                             </div>
                             <div>{handleDatetime(app.datetime)}</div>
                             <div class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" on:click={() => handleClick(`/employees/${app.employeeId}`)}>
-                                {app.employeeFio?.split(' ')[0]}
+                                {app.employeeFio}
                             </div>
                             <div class="flex justify-center w-full">
                                 {@html handleStatus(app.status)}
