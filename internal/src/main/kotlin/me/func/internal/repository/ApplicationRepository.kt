0618e13f670309a8b1559bd3d4877a8f344d8d2d
funcid <rtem903134@gmail.com>
2024-06-24 11:15:24 +0300

    fix empty allocation

--- internal/src/main/kotlin/me/func/internal/repository/ApplicationRepository.kt
+++ internal/src/main/kotlin/me/func/internal/repository/ApplicationRepository.kt
@@ -16,53 +16,51 @@ interface ApplicationRepository : CrudRepository<Application, Long> {
     fun findByStatus(status: ApplicationStatus): List<Application>
 
     @Query("""
-    SELECT 
-        a.id, 
-        a.id_pas as idPas,
-        a.time3,
-        a.time4,
-        a.time_over as timeOver, 
-        a.status, 
-        a.cat_pas as catPas,
-        a.datetime, 
-        COALESCE(p.full_name, 'ФИО не найдено') AS fullName, 
-        c.number as mobileNumber,
-        a.tpz,
-        ms1.id as stationFromId,
-        ms1.name_station as stationFromName,
-        ms1.name_line as stationFromLine,
-        ms1.id_line as stationFromLineId,
-        ms2.id as stationToId,
-        ms2.name_station as stationToName,
-        ms2.name_line as stationToLine,
-        ms2.id_line as stationToLineId,
-        alloc.employee_fio as employeeFio, 
-        alloc.employee_id as employeeId
-    FROM 
-        applications a
-    LEFT JOIN 
-        passenger p ON a.id_pas::bigint = p.id
-    LEFT JOIN 
-        contact_numbers c ON p.id = c.passenger_id
-    LEFT JOIN 
-        allocation alloc ON a.id = alloc.application_id
-    LEFT JOIN 
-        metro_stations ms1 ON a.id_st1 = ms1.id
-    LEFT JOIN 
-        metro_stations ms2 ON a.id_st2 = ms2.id
-    WHERE 
-        (:namePrefix IS NULL OR LOWER(COALESCE(p.full_name, 'ФИО не найдено')) LIKE LOWER(CONCAT(:namePrefix, '%')))
-        AND a.datetime BETWEEN :startOfDay AND :endOfDay
-        AND alloc.type = 'APPLICATION'
-    ORDER BY 
-        a.datetime DESC
-""", nativeQuery = true)
+        SELECT 
+            a.id, 
+            a.id_pas as idPas,
+            a.time3,
+            a.time4,
+            a.time_over as timeOver, 
+            a.status, 
+            a.cat_pas as catPas,
+            a.datetime, 
+            COALESCE(p.full_name, 'ФИО не найдено') AS fullName, 
+            c.number as mobileNumber,
+            a.tpz,
+            ms1.id as stationFromId,
+            ms1.name_station as stationFromName,
+            ms1.name_line as stationFromLine,
+            ms1.id_line as stationFromLineId,
+            ms2.id as stationToId,
+            ms2.name_station as stationToName,
+            ms2.name_line as stationToLine,
+            ms2.id_line as stationToLineId,
+            alloc.employee_fio as employeeFio, 
+            alloc.employee_id as employeeId
+        FROM 
+            applications a
+        LEFT JOIN 
+            passenger p ON a.id_pas::bigint = p.id
+        LEFT JOIN 
+            contact_numbers c ON p.id = c.passenger_id
+        LEFT JOIN 
+            allocation alloc ON a.id = alloc.application_id AND alloc.type = 'APPLICATION'
+        LEFT JOIN 
+            metro_stations ms1 ON a.id_st1 = ms1.id
+        LEFT JOIN 
+            metro_stations ms2 ON a.id_st2 = ms2.id
+        WHERE 
+            (:namePrefix IS NULL OR LOWER(COALESCE(p.full_name, 'ФИО не найдено')) LIKE LOWER(CONCAT(:namePrefix, '%')))
+            AND a.datetime BETWEEN :startOfDay AND :endOfDay
+        ORDER BY 
+            a.datetime DESC
+    """, nativeQuery = true)
     fun findApplicationsByDateAndPassengerNamePrefix(
         @Param("startOfDay") startOfDay: LocalDateTime,
         @Param("endOfDay") endOfDay: LocalDateTime,
         @Param("namePrefix") namePrefix: String
     ): List<ApplicationPassengerInfo>
-
 
     fun findAllByDatetimeBetween(from: Timestamp, to: Timestamp): List<Application>
 
--- web/src/components/applications/Applications.svelte
+++ web/src/components/applications/Applications.svelte
@@ -158,9 +158,13 @@
                                 {app.stationTo?.nameStation}
                             </div>
                             <div>{handleDatetime(app.datetime)}</div>
-                            <div class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" on:click={() => handleClick(`/employees/${app.employeeId}`)}>
-                                {app.employeeFio}
-                            </div>
+                            {#if app.employeeFio}
+                                <div class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" on:click={() => handleClick(`/employees/${app.employeeId}`)}>
+                                    {app.employeeFio}
+                                </div>
+                            {:else}
+                                Нет
+                            {/if}
                             <div class="flex justify-center w-full">
                                 {@html handleStatus(app.status)}
                             </div>
